a40850eff25ead07e0703a4cbb78c0499926f1b1
Josh Rosen <joshrosen@databricks.com>
2016-04-02 11:21:02 -0700

    Towards emulating Java's null semantics.

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -1861,9 +1861,8 @@ class Dataset[T] private[sql](
    */
   @Experimental
   def filter(func: T => Boolean): Dataset[T] = {
-    val res = ClosureToExpressionConverter.convert(func, schema).map { expr =>
-      println(s"inferred expression is $expr")
-      where(Column.apply(Cast(expr, BooleanType)))
+    val res = ClosureToExpressionConverter.convertFilter(func, schema).map { expr =>
+      where(Column(expr))
     }.getOrElse {
       mapPartitions(_.filter(func))
     }
